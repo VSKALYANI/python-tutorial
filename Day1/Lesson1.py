a11ab2d64644acd9d9d11a012ec5cd439daa2a0a
VSKALYANI <82025460+VSKALYANI@users.noreply.github.com>
2021-04-07 15:35:31 +0530

    Addition

---
+++ Day1/Lesson1.py
@@ -0,0 +1,7 @@
+def addition(param1, param2):
+    print(param1 + param2)
+
+
+addition(5, 6)
+
+addition("Suhrith", " Nishritha")
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-tutorial
-This is my python exercises
+This is my python exercise
+This is the second day of practice
